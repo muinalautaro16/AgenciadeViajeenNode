a2c82cd3966a545a3833654ae9a70dc4775df546
Lautaro Muiña <87960189+muinalautaro16@users.noreply.github.com>
2024-02-22 04:12:54 -0300

    + Secciones agregadas y seccion de nosotros lista

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,28 @@ import  express  from "express";
 const router = express.Router();
 
 router.get('/', (req, res ) => { // Req - lo que enviamos : res - lo que express nos responde
-    res.send('Inicio');
+    res.render('Inicio', {
+        pagina: 'Inicio'
+    });
+
 });
+
 router.get('/nosotros', (req, res ) => { // Req - lo que enviamos : res - lo que express nos responde
-
-    const viajes = 'Viaje a Argentina';
-
     res.render('nosotros', {
-        viajes,
+        pagina: 'Nosotros'
     });
 });
-router.get('/contacto', (req, res ) => { // Req - lo que enviamos : res - lo que express nos responde
-    res.send('Contacto');
+
+router.get('/viajes', (req, res ) => { // Req - lo que enviamos : res - lo que express nos responde
+    res.render('viajes', {
+        pagina: 'Viajes'
+    });
+});
+
+router.get('/testimoniales', (req, res ) => { // Req - lo que enviamos : res - lo que express nos responde
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    });
 });
 
 export default router;
